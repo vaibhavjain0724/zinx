66d4bf4ecc88ff3bc6ec1df19ab9d4a3cfcf2cbf
Vaibhav Jain <vaibhavjain0724@gmail.com>
2026-08-12 15:38:13 +0530

    interpreter handles binary expressions

--- src/interpreter/interpreter.js
+++ src/interpreter/interpreter.js
@@ -1,28 +1,64 @@
-const ast = {
-    type: "BinaryExpression",
-    operator: "+",
+// const ast = {
+//     type: "BinaryExpression",
+//     operator: "+",
 
-    left: {
-        type: "IntegerLiteral",
-        value: 5
-    },
+//     left: {
+//         type: "IntegerLiteral",
+//         value: 5
+//     },
 
-    right: {
-        type: "IntegerLiteral",
-        value: 10
-    }
-};
+//     right: {
+//         type: "IntegerLiteral",
+//         value: 10
+//     }
+// };
+
+// let outputArr = [];
+
+
+// function interpret(node) {  
+//     if (node.type === "IntegerLiteral") {
+//         return node.value;
+//     }
+//     if (node.type === "ShowStatement") {
+//         const value = interpret(node.expression);
+//         outputArr.push(value);
+//     }
+
+//     if (node.type === "BinaryExpression") {
+
+//         const left = interpret(node.left);
+//         const right = interpret(node.right);
+
+//         if (node.operator === "+") {
+//             return left + right;
+//         }
+//     }
+// }
+
+// console.log(interpret(ast))
+
+
+
+
+
+// export {interpret , outputArr}
+
+
+
 
 let outputArr = [];
 
+function interpret(node) {
 
-function interpret(node) {  
     if (node.type === "IntegerLiteral") {
         return node.value;
     }
+
     if (node.type === "ShowStatement") {
         const value = interpret(node.expression);
         outputArr.push(value);
+        return;
     }
 
     if (node.type === "BinaryExpression") {
@@ -33,13 +69,23 @@ function interpret(node) {
         if (node.operator === "+") {
             return left + right;
         }
+
+        if (node.operator === "-") {
+            return left - right;
+        }
+
+        if (node.operator === "*") {
+            return left * right;
+        }
+
+        if (node.operator === "/") {
+            if (right === 0) {
+                throw new Error("Division by zero");
+            }
+
+            return left / right;
+        }
     }
 }
 
-console.log(interpret(ast))
-
-
-
-
-
-export {interpret , outputArr}
+export { interpret, outputArr };
